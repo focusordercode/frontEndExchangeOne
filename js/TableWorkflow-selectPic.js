960246a71b29aad3061518dcafb901de37e7453a
nicegonice <runpad@163.com>
2016-10-24 18:33:04 +0800

    2016.10.24 18:32

--- js/TableWorkflow-selectPic.js
+++ js/TableWorkflow-selectPic.js
@@ -21,6 +21,7 @@ var type_code = 'info';
 
 var serverUrl = "http://192.168.1.42/canton/"; //后端接口地址
 var oUrl = 'http://192.168.1.42/canton';//图片服务器地址
+var search = "http://192.168.1.42/canton/index.php/vague/name"; //模糊搜索地址
 
 // 图片目录树形菜单的组件
 Vue.component('tree', {
@@ -72,7 +73,17 @@ var selectPic = new Vue({
         num_now:'',
         not_enough:0,
         count:'',
-        rand_id:''
+        rand_id:'',
+        seeMore:false,
+        //产品
+        proList:'', //类目
+        relateList:'', //产品
+        pro:'',
+        relate:'',
+        relateValList:[],
+        //整合了的表格数据
+        tableHead:'',
+        tableData:''
     },
     ready:function(){
         //获取表格信息
@@ -105,11 +116,36 @@ var selectPic = new Vue({
         },
         //控制进入下一步骤按钮
         NextStepStatus:function(){
-            var picDataLen = this.picData.length;
-            if(picDataLen>0){
+            var headLen = this.tableHead.length;
+            var tableDataLen = this.tableData.length;
+            if(headLen>0&&tableDataLen>0){
                 return false
             }else{
                 return true
+            }
+        },
+        //控制选择产品区域
+        pro_zone:function () {
+            if(this.picData&&this.not_enough==0){
+                return false
+            }else{
+                return true
+            }
+        },
+        //匹配按钮
+        fitBtn:function () {
+            if (this.relateValList.length>0) {
+                return false
+            }else{
+                return true
+            }
+        },
+        //图片展示表格显示
+        PicTable:function () {
+            if (this.picData.length>0&&this.tableHead.length<1) {
+                return true
+            }else{
+                return false
             }
         }
     },
@@ -151,6 +187,10 @@ var selectPic = new Vue({
             this.re_date = 3;
             this.file_type = 'jpg';
         },
+        //查看更多
+        toSeeMore:function () {
+            this.seeMore = true;
+        },
         //请求并获取筛选的图片
         searchPic:function(){
             var vm = this;
@@ -164,6 +204,7 @@ var selectPic = new Vue({
             if(!gallery_id){
                 layer.msg('请先选择图片目录');
             }else{
+                var LoadIndex = layer.load(3, {shade:[0.3, '#000']}); //开启遮罩层
                 //获取图片信息
                 $.ajax({
                     type:'POST',
@@ -180,44 +221,182 @@ var selectPic = new Vue({
                         file_type:file_type
                     },
                     success:function(data){
+                        layer.close(LoadIndex); //关闭遮罩层
                         if(data.status==100){
                             layer.msg('筛选成功');
-                            selectPic.picData = data.value;
-                            selectPic.num_now = data.num_now;
-                            selectPic.not_enough = data.not_enough;
-                            selectPic.count = data.count;
-                            selectPic.rand_id = data.rand_id;
+                            vm.picData = data.value;
+                            vm.num_now = data.num_now;
+                            vm.not_enough = data.not_enough;
+                            vm.count = data.count;
+                            vm.rand_id = data.rand_id;
+                            //清空数据
+                            vm.tableHead = '';
+                            vm.tableData = '';
+                            vm.relateValList = [];
                         }else{
                             layer.msg(data.msg);
-                            selectPic.picData= '';
-                            selectPic.num_now = '';
-                            selectPic.not_enough = '';
-                            selectPic.count = '';
-                            selectPic.rand_id = '';
+                            vm.picData= '';
+                            vm.num_now = '';
+                            vm.not_enough = '';
+                            vm.count = '';
+                            vm.rand_id = '';
+                            //清空数据
+                            vm.tableHead = '';
+                            vm.tableData = '';
+                            vm.relateValList = [];
                         }
                     },
                     error:function(jqXHR){
+                        layer.close(LoadIndex); //关闭遮罩层
                         layer.msg('向服务器获取请求图片失败');
                     }
                 })
             }
         },
-        //跳转上传步骤
-        NextStep:function(){
-            var not_enough = this.not_enough;
-            if(not_enough!=0){
-                layer.msg('图片不足');
+        //从搜索结果中选中一个类目
+        selectCate:function(pro){
+            this.pro = pro;
+            this.proList = '';
+            //清空产品
+            this.relate = '';
+            //清除值，隐藏框
+            $('.searchCate').val('');
+            $('.searchCompent').hide();
+        },
+        //从产品搜索结果选择一个产品
+        selectRelate:function (relate) {
+            this.relate = relate;
+            this.relateList = '';
+            //清除值，隐藏框
+            $('.searchCate2').val('');
+            $('.searchCompent2').hide();
+        },
+        //添加关联
+        goRelate:function () {
+            var vm = this;
+            if (!(this.relate.id)) {
+                layer.msg('类目和产品必须要都选择');
             }else{
-                var url = 'TableWorkflow-upload.html';
-                var tableID = selectPic.tableInfo.id;
-                var rand_id = selectPic.rand_id;
-                var pic_rate = selectPic.pic_rate;
-                var pro_rate = selectPic.pro_rate;
-                window.location.href = url+'?tableID='+tableID+'&rand_id='+rand_id+'&pic_rate='+pic_rate+'&pro_rate='+pro_rate;
+                var obj = {};
+                obj.category_cn = vm.pro.cn_name;
+                obj.category_en = vm.pro.en_name;
+                obj.cn_name = vm.relate.cn_name;
+                obj.en_name = vm.relate.en_name;
+                obj.id = vm.relate.id;
+                vm.relateValList.push(obj);
+                //清空产品
+                vm.relate = '';
+            }
+        },
+        //获取匹配的词库
+        fitData:function () {
+            var vm = this;
+            if (vm.relateValList.length>0) {
+                var count = vm.count;
+                var idArr = vm.relateValList.slice();
+                var picData = vm.picData.slice();
+                dataSteam(count,idArr,picData,selectPic);
+            }else{
+                layer.msg('请先选择产品');
+            }
+        },
+        //删除关联
+        deleteRelate:function (rList) {
+            this.relateValList.$remove(rList);
+        },
+        //跳转到编辑步骤
+        NextStep:function () {
+            var vm = this;
+            if (vm.tableData.length<1) {
+                layer.msg('请先选择图片匹配完成');
+            }else{
+                $.ajax({
+                    type:'POST',
+                    url:serverUrl+'receive/value',
+                    datatype:'json',
+                    data:{
+                        data:vm.tableInfo.form_no,
+                        form_no:vm.tableData
+                    },
+                    success:function(data){
+                        if(data.status==100){
+                            layer.msg('请求成功');
+                            //跳转函数
+                            function goNext() {
+                                var url = 'TableWorkflow-edit.html';
+                                window.location.href = url+'?id='+Id;
+                            }
+
+                            setInterval(goNext,1000);
+                        }else{
+                            layer.msg(data.msg);
+                        }
+                    },
+                    error:function(jqXHR){
+                        layer.msg('向服务器请求失败');
+                    }
+                })
             }
         }
     }
 })
+
+
+//获取产品所关联的词库内容并合并数据的函数
+function dataSteam (num,idArr,picData,vm) {
+    var arr = [],
+        header = [], //表格的表头总数据
+        wordData = [], //表格的总数据
+        picAdress = [];
+    var defaultHead = ['photo'];
+
+    for (var i = 0;i<idArr.length;i++) {
+        arr.push(idArr[i].id)
+    }
+
+    for (var h = 0;h<picData.length;h++) {
+        picAdress.push(picData[h].path+'/'+picData[h].file_name);
+    }
+
+
+    if (arr.length>0) {
+        var LoadIndex = layer.load(3, {shade:[0.3, '#000']}); //开启遮罩层
+        $.ajax({
+            type:'POST',
+            url:serverUrl+'get/good2centervalue',
+            datatype:'json',
+            data:{
+                good_id:arr,
+                num:num
+            },
+            success:function(data){
+                layer.close(LoadIndex); //关闭遮罩层
+                if(data.status==100){
+                    //整合表头
+                    var getHead = data.header;
+                    header = defaultHead.concat(getHead);
+
+                    wordData = data.value;
+                    //把图片数据加进去
+                    for(var y = 0;y<picAdress.length;y++){
+                        wordData[y].photo=(picAdress[y]);
+                    }
+
+                    console.log(wordData);
+                    //赋值给vue实例
+                    vm.tableHead = header;
+                    vm.tableData = wordData;
+                }else{
+                    layer.msg(data.msg);
+                }
+            },
+            error:function(jqXHR){
+                layer.close(LoadIndex); //关闭遮罩层
+                layer.msg('向服务器请求词库数据失败');
+            }
+        })
+    }
+}
 
 Vue.filter('sizeCounter',function(value){
     var str = value;
@@ -234,9 +413,91 @@ Vue.filter('imgUrl',function(value){
     return strNew
 })
 
+Vue.filter('imgUrl2',function(value){
+    var str = value;
+    var strLen = str.length;
+    var strNew = str.substr(1,strLen-1);
+    strNew = oUrl + strNew;
+    return strNew
+})
 
 //点击图片目录树形菜单
 $(document).on('click','.tree .item .label',function(){
     $('.tree .item .label').removeClass('label-success').addClass('label-primary');
     $(this).removeClass('label-primary').addClass('label-success');
 });
+
+$(function(){
+    //搜索类目框
+    $('.searchBtn').on('click',function(){
+        $('.searchCompent').show();
+        $('.searchCompent2').hide();
+    })
+    $('.closeBtn').on('click',function(){
+        $('.searchCompent').hide();
+    })
+
+    //搜索产品框
+    $('.searchBtn2').on('click',function(){
+        $('.searchCompent2').show();
+        $('.searchCompent').hide();
+    })
+    $('.closeBtn2').on('click',function(){
+        $('.searchCompent2').hide();
+    })
+})
+
+//模糊搜索类目
+$('.searchCate').on('keyup',function(){
+    var getWidth = $('.pors .cate-list').prev('.form-control').innerWidth();
+    $('.pors .cate-list').css('width',getWidth);
+    var searchCusVal = $('.searchCate').val();
+
+    $.ajax({
+        type:'POST',
+        url:search,
+        datatype:'json',
+        data:{
+            text:searchCusVal
+        },
+        success:function(data){
+            if(data.status==100){
+                selectPic.proList = data.value;
+            }else{
+                selectPic.proList= '';
+            }
+        },
+        error:function(jqXHR){
+            layer.msg('向服务器请求客户信息失败');
+        }
+    })
+});
+
+//搜索产品
+$('.searchCate2').on('keyup',function(){
+    var getWidth = $('.searchCate2').innerWidth();
+    $('.searchCate2').next().css('width',getWidth);
+    var searchCusVal = $('.searchCate2').val();
+
+    $.ajax({
+        type:'POST',
+        url:serverUrl+'get/allproductcenter',
+        datatype:'json',
+        data:{
+            category_id:selectPic.pro_id,
+            enabled:1,
+            vague:searchCusVal
+        },
+        success:function(data){
+            if(data.status==100){
+                selectPic.relateList = data.value;
+            }else{
+                selectPic.relateList= '';
+            }
+        },
+        error:function(jqXHR){
+            layer.msg('向服务器请求失败');
+        }
+    })
+});
+
