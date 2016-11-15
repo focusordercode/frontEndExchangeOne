648a76013f435d534ea2ed2a7028566e0bf0e39b
nicegonice <runpad@163.com>
2016-11-15 18:20:48 +0800

    2016.11.15 18:20

--- js/TableWorkflow-edit-new.js
+++ js/TableWorkflow-edit-new.js
@@ -27,7 +27,7 @@ console.log(serverUrl); //后端接口地址
 var oUrl = serverUrl;//图片服务器地址
 
 //未提交保存内容提示
-// $(window).bind('beforeunload',function(){return "您修改的内容尚未保存，确定离开此页面吗？";});
+$(window).bind('beforeunload',function(){return "您修改的内容尚未保存，确定离开此页面吗？";});
 
 
 var pageSize = 100;//默认每页展示多少数据，加载时用,修改这里时把data的pageSize一起改
@@ -158,23 +158,6 @@ var oTableIn = new Vue({
             }else{
                 return false
             }
-        },
-        //数据检查前往修改按钮
-        goXgCheckBtn:function () {
-            var checkRultData = [], checkRultData = this.checkRultData;
-            var arr = [];//收集收据数组
-            for (var i = 0;i<checkRultData.length;i++) {
-                var str = '数据无误';
-                if (checkRultData[i].value ==str) {
-                    arr.push(i);
-                }
-            }
-
-            if(arr.length==checkRultData.length){
-                return true //全部通过
-            }else{
-                return false //有未通过项
-            }
         }
     },
     methods:{
@@ -296,46 +279,18 @@ var oTableIn = new Vue({
                     }
                 })
             }
-        },
-        //发送数据检查请求
-        checkRequest:function () {
-            var vm = this;
-            var LoadIndex = layer.load(3, {shade:[0.3, '#000']}); //开启遮罩层
-            $.ajax({
-                type:'POST',
-                url:serverUrl+'data/check',
-                datatype:'json',
-                data:{
-                    form_id:vm.info.id,
-                    type_code:type_code,
-                    check:vm.checkData
-                },
-                success:function(data){
-                    console.log(data);
-                    layer.close(LoadIndex); //关闭遮罩层
-                    if(data.status==100){
-                        layer.msg('请求成功');
-                        vm.checkRultData = data.value;
-                    }else{
-                        layer.msg(data.msg);
-                    }
-                },
-                error:function(jqXHR){
-                    layer.close(LoadIndex); //关闭遮罩层
-                    layer.msg('向服务器请求失败');
-                }
-            })
-        },
-        //前往修改数据
-        goXgCheck:function () {
-            var vm = this,
-                template_id = vm.info.template_id;
-            if (template_id&&tableID) {
-                var url = 'changeTableData.html?id='+tableID+'&template_id='+template_id+'&type_code='+type_code;
-                window.open(url);
-            }
         }
     }
+})
+
+//数据检查类型
+Vue.filter('ruleType', function (value) {
+    var str;
+    switch(value){
+        case 1: str = "唯一";break;
+        case 2: str = "重复";break;
+    }
+    return str;
 })
 
 $(document).ready(function(){
@@ -449,7 +404,7 @@ $(document).ready(function(){
     getAllData ();
 
     // console.log(headers)
-    console.log(gridData)
+    // console.log(gridData)
     // console.log(cols)
 
     //handsontable实例
@@ -468,7 +423,7 @@ $(document).ready(function(){
         // colWidths:150,
         // rowHeights:30,
         width:1400,
-        height:800,
+        height:700,
         autoRowSize: true,
         autoColSize: false,
         fixedColumnsLeft: 5
@@ -536,97 +491,61 @@ $(document).ready(function(){
                             oDelete.push(i);
                         }
                     }
-                    var deleteLen = oDelete.length + 1;//统计删除的个数,加上主体本身
+                    var deleteLen = oDelete.length;//统计删除的个数
+                    if(deleteLen){
+                        deleteLen = deleteLen + 1;//加上主体本身
+                    }else{
+                        deleteLen = 1;
+                    }
+                    
                     hot.alter('remove_row',rowIndex[0],deleteLen);//删除主体及其变体方法
                     layer.msg('删除成功',{time:1000});
                 }
-            }else{
-                layer.msg('未知错误');
             }
         }
     })
 
-    //获取product_id的函数
-    function getId() {
-        var a;
-        $.ajax({
-            type:'POST',
-            url:serverUrl+'get/sysId',
-            datatype:'json',
-            async:false,
-            data:{
-                app_code:'product_information',
-                num:1
-            },
-            success:function(data){
-                if(data.status==100){
-                    a = data.value[0];
-                }else if(data.status==101){
-                    layer.msg('操作失败');
-                }else{
-                    layer.msg(data.msg);
-                }
-            },
-            error:function(jqXHR){
-                layer.msg('向服务器请求增加失败');
+    //发起数据检查请求
+    $(document).on('click','.gocheck',function(){
+        var Len = headers.length;//表头长度
+        var checkName = $(this).closest('.form-group').find('.control-label').eq(0).text();//检查项
+        var checkType = $(this).closest('.form-group').find('.form-control').eq(0).val();//检查类型
+            checkType = Number(checkType);
+        var checkIndex;//检查项在表头中的位置
+
+        //获取下标
+        for(var h = 0;h<Len;h++){
+            if(checkName==headers[h]){
+                checkIndex = h;
             }
-        })
-        return a
-    }
-
-    //发起删除请求
-    function dRequest(product_id) {
-        var a;
-        $.ajax({
-            type:'POST',
-            url:serverUrl+'delete/product',
-            datatype:'json',
-            async:false,
-            data:{
-                type_code:type_code,
-                product_id:product_id
-            },
-            success:function(data){
-                if(data.status==100){
-                    a = 1;//删除成功
+        }
+        console.log(checkIndex);
+        //进入数据检查
+        if(checkType&&checkIndex){
+            //判断检查的类型
+            if(checkType==1){ //唯一性检查
+                hot.render();
+                var arr = hot.getDataAtCol(checkIndex);//获取检查数据
+                var arrRpt = checkOnly(arr);//执行检查函数获取结果
+                if(arrRpt.length){
+                    hot.selectCell(arrRpt[0],checkIndex);//定位到第一个数据重复的数据
+                    layer.msg('已定位到第一个重复的单元格',{time:1000});
                 }else{
-                    a = 2;//删除失败
-                    layer.msg(data.msg);
+                    layer.msg('检查唯一性通过');
                 }
-            },
-            error:function(jqXHR){
-                a = 2;//删除失败
-                layer.msg('向服务器请求删除失败');
+            }else if(checkType==2){ //重复性检查
+                hot.render();
+                var arr1 = hot.getDataAtCol(checkIndex);//获取检查数据
+                var arrRpt = checkRepeat(arr1)//执行检查函数获取结果
+                if(arrRpt){
+                    hot.selectCell(arrRpt,checkIndex);//定位到第一个数据不重复的数据
+                    layer.msg('已定位到第一个不重复的单元格',{time:1000});
+                }else{
+                    layer.msg('检查重复性通过');
+                }
             }
-        })
-        return a
-    }
-
-    //photo
-    function coverRenderer (instance, td, row, col, prop, value, cellProperties) {
-       var escaped = Handsontable.helper.stringify(value),
-         img;
-     
-       if (escaped.indexOf('http') === 0) { //该链接字符串http首次出现的位置是0的情况下
-         img = document.createElement('IMG');
-         img.src = value;
-         img.width = 50;
-         img.height = 50;
-
-         Handsontable.Dom.addEvent(img, 'mousedown', function (e){
-           e.preventDefault(); // prevent selection quirk
-         });
-     
-         Handsontable.Dom.empty(td);
-         td.appendChild(img);
-       }
-       else {
-         // render as text
-         Handsontable.renderers.TextRenderer.apply(this, arguments);
-       }
-     
-       return td;
-     }
+        }
+    });
 
     //提交暂存
     $('.cacheData').on('click',function(){
@@ -660,7 +579,7 @@ $(document).ready(function(){
 
                     layer.msg('暂存成功');
                     //解除未提交内容提示
-                    // $(window).unbind('beforeunload');
+                    $(window).unbind('beforeunload');
 
                     setInterval(windowFresh,1000);
                 }else{
@@ -674,13 +593,185 @@ $(document).ready(function(){
         })
     })
 
-    //检测滚动条位置，显示隐藏页面头部
-    // $(window).scroll(function(){
-    //    if($(window).scrollTop() > 50){
-    //        $('.panel-top').slideUp(300);
-    //        $('.pullDown').show();
-    //    }
+    //提交数据跳转
+    $('.submitData').on('click',function(){
+        hot.render();//获取前先把表格渲染一次,防止修改的没有获取到
+        tableData = hot.getData();
+        console.log(tableData);
+        console.log(gridData);
+        var gridColumns = headers.slice();
+        console.log(gridColumns);
+        //提交
+        var vm = oTableIn;
+        var max = tableData.length;
+        console.log(type_code);
+        layer.load(3, {shade:[0.3, '#000']}); //开启遮罩层
+        $.ajax({
+            type:'POST',
+            url:serverUrl+'commit/data',
+            datatype:'json',
+            data:{
+                type:'submit',
+                category_id:vm.info.category_id,
+                template_id:vm.info.template_id,
+                form_id:vm.info.id,
+                type_code:type_code,
+                max:max,
+                gridColumns:gridColumns,
+                gridData:tableData
+            },
+            success:function(data){
+                layer.closeAll(); //关闭遮罩层
+                if (data.status==100) {
+
+                    layer.msg('提交成功');
+
+                    //解除未提交内容提示
+                    $(window).unbind('beforeunload');
+                    
+                    var url = 'TableWorkflow-done.html';
+                    var tableID = vm.info.id;
+                    var tem_id = vm.info.template_id;
+                    //跳转到下一步
+                    if(tableID&&tem_id){
+                        setInterval(goNext,1000)
+                    }
+                    function goNext(){
+                        window.location.href = url+'?id='+tableID+'&template_id='+tem_id;
+                    }
+                }else{
+                    layer.msg(data.msg);
+                }
+            },
+            error:function(jqXHR){
+                layer.closeAll(); //关闭遮罩层
+                layer.msg('向服务器请求暂存失败');
+            }
+        })
+    })
+
+    //获取product_id的函数
+    function getId() {
+        var a;
+        $.ajax({
+            type:'POST',
+            url:serverUrl+'get/sysId',
+            datatype:'json',
+            async:false,
+            data:{
+                app_code:'product_information',
+                num:1
+            },
+            success:function(data){
+                if(data.status==100){
+                    a = data.value[0];
+                }else if(data.status==101){
+                    layer.msg('操作失败');
+                }else{
+                    layer.msg(data.msg);
+                }
+            },
+            error:function(jqXHR){
+                layer.msg('向服务器请求增加失败');
+            }
+        })
+        return a
+    }
+
+    //发起删除请求函数
+    function dRequest(product_id) {
+        var a;
+        $.ajax({
+            type:'POST',
+            url:serverUrl+'delete/product',
+            datatype:'json',
+            async:false,
+            data:{
+                type_code:type_code,
+                product_id:product_id
+            },
+            success:function(data){
+                if(data.status==100){
+                    a = 1;//删除成功
+                }else{
+                    a = 2;//删除失败
+                    layer.msg(data.msg);
+                }
+            },
+            error:function(jqXHR){
+                a = 2;//删除失败
+                layer.msg('向服务器请求删除失败');
+            }
+        })
+        return a
+    }
+
+    //数据查重函数(唯一性)
+    function checkOnly(arr) {
+        var arrRpt = [], hash = {};   
+        for (var i = 0, elem; (elem = arr[i]) != null; i++) {
+            if (!hash[elem]) {
+                hash[elem] = true;
+            }else{
+                arrRpt.push(i);//获取重复 
+            }
+        }
+        return arrRpt
+    }
+    //数据查重复函数(重复性)
+    function checkRepeat(arr) {
+        var arrRpt;
+        for(var i = 0;i<arr.length;i++){
+            if(i+1==arr.length){
+                break
+            }else{
+                if(arr[i]!=arr[i+1]){
+                    arrRpt = i+1;
+                    break
+                }
+            }
+        }
+        console.log(arrRpt);
+        return arrRpt
+    }
+
+    //photo
+    function coverRenderer (instance, td, row, col, prop, value, cellProperties) {
+       var escaped = Handsontable.helper.stringify(value),
+         img;
+     
+       if (escaped.indexOf('http') === 0) { //该链接字符串http首次出现的位置是0的情况下
+         img = document.createElement('IMG');
+         img.src = value;
+         img.className = 'imgcols';
+         img.width = 50;
+         img.height = 50;
+
+         Handsontable.Dom.addEvent(img, 'mousedown', function (e){
+           e.preventDefault(); // prevent selection quirk
+         });
+     
+         Handsontable.Dom.empty(td);
+         td.appendChild(img);
+       }
+       else {
+         // render as text
+         Handsontable.renderers.TextRenderer.apply(this, arguments);
+       }
+     
+       return td;
+     }
+
+     //图片放大展示
+    // $(document).on('mouseenter','.imgcols',function(){
+    //     console.log($(this).attr('src'));
     // })
+
+    //刷新函数
+    function windowFresh() {
+        location.reload(true);
+    }
+
 
     $('.pullUP').click(function(){
         $('.panel-top').slideUp(300);
@@ -697,8 +788,3 @@ $(document).ready(function(){
         $("html,body").animate({scrollTop:0},300);
     });
 });
-
-//刷新函数
-function windowFresh() {
-    location.reload(true);
-}
