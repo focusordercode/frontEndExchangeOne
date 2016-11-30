fd28166778dc83ce30ec3a0cc97cbb0ab2b10ebe
zhang <1336563477@qq.com>
2016-11-30 13:55:17 +0800

    用户增加页面、详情页面

--- js/add_user.js
+++ js/add_user.js
@@ -34,34 +34,33 @@ var adduse = new Vue ({
         //添加用户
         adduserbtn:function () {
             var vm = adduse;
-            if (!(vm.user_name.trim())){
+            if (!(vm.user_name.trim())) {
                 vm.al_name = true;
-            }else if (!(vm.password.trim())){
+            } else if (!(vm.password.trim())) {
                 vm.al_name = false;
                 vm.al_pass = true;
-            }else if (!vm.real_name){
+            } else if (!vm.real_name) {
                 vm.al_name = false;
                 vm.al_pass = false;
                 vm.al_true = true;
-            }else if (!vm.belong){
+            } else if (!vm.belong) {
                 vm.al_name = false;
                 vm.al_pass = false;
                 vm.al_true = false;
                 vm.al_belong = true;
-            }else {
+            } else {
                 vm.al_name = false;
                 vm.al_pass = false;
                 vm.al_true = false;
                 vm.al_belong = false;
 
                 $.ajax({
-                    type:'POST',
-                    url:'',
-                    datatype:'json',
-                    data:{
-
-                },
-            })
+                    type: 'POST',
+                    url: '',
+                    datatype: 'json',
+                    data: {},
+                })
+            }
         }
     }
 })
